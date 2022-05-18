80f879480997aba66384a34351c7a59dabdfdabb
technical-programmer <92447655+technical-programmer@users.noreply.github.com>
2022-05-18 16:20:58 +0530

    Update no_pyramid.cpp
    no_pyramid is updated by ankit

--- patterns/no_pyramid.cpp
+++ patterns/no_pyramid.cpp
@@ -6,7 +6,7 @@ int main()
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        for (int s = i; s <= n - i; s++)
+        for (int s = i; s <= n - i; s++) //this loop is for spaces
         {
             cout << " ";
         }
